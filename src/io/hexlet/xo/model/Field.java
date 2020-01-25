8ee3bd6d6c3bd79cc07f632ee3f21a673ed6781f
Test Name <test@email.ru>
2020-01-25 22:27:36 +0300

    Field cheks added

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,7 +1,7 @@
 package io.hexlet.xo.model;
 
 import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
-import io.hexlet.xo.model.exceptions.InvalidePointException;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
@@ -16,19 +16,21 @@ public class Field {
         return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point) throws InvalidePointException {
+    public Figure getFigure(final Point point) throws InvalidPointException {
+
         if (!checkPoint(point)){
-            throw new InvalidePointException();
+            throw new InvalidPointException();
         }
+
         return field[point.x][point.y];
 
     }
 
-    public void setFigure(final Point point,final Figure figure) throws InvalidePointException,
+    public void setFigure(final Point point,final Figure figure) throws InvalidPointException,
                                                                         AlreadyOccupiedException {
 
         if (!checkPoint(point)){
-            throw new InvalidePointException();
+            throw new InvalidPointException();
         }
 
         if (field[point.x][point.y] != null) {
